afc67cbe9e9ec9eae9e6e33d60faf6a3fa808bee
anyatamaraa <anyatmr@gmail.com>
2022-11-13 10:41:09 +0000

    - add csv file - show table

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 # landing page: Control how the UI look like alongside with each features functionality 
 from flask import Flask, render_template
-from flask_bootstrap import Bootstrap5
+import csv
 
 app = Flask(__name__)
-bootstrap = Bootstrap5(app)
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    upload_file = "Upload file here"
-    return render_template('landing_page.html', upload_file= upload_file)
+    # Showing the .csv file
+    with open('./csv.csv') as file:
+        reader = csv.reader(file)
+        return render_template('landing_page.html', csv=reader)
 
 if __name__ == '__main__':
     app.run(debug=True)
